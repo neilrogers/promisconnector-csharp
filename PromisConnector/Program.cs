517e63ec5121ef0ba4c37b9f1ae47949c9c5cdeb
Neil Rogers <neil.rogers@jetconvert.com>
2020-06-12 13:07:45 +1000

    Initial commit

---
+++ PromisConnector/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace PromisConnector
+{
+    class Program
+    {
+        Promis promis = null;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.promis = new Promis();
+            
+            Console.WriteLine(program.promis.config()["username"]);
+
+            //dynamic register = program.promis.register("orgname", "emailaddress", password, "FirstName", "MiddleName", "LastName");
+
+            program.promis.login(program.promis.config()["username"], program.promis.config()["password"], program.promis.config()["entityid"]);
+            dynamic message = program.promis.getConnector();
+            Console.WriteLine(message);
+
+            program.sendTaxCodes();
+            program.sendContacts();
+            program.sendInvoice();
+
+        }
+
+
+        dynamic sendTaxCodes()
+        {
+            string xml = @"<TaxCodes>
+                <TaxCode>
+                    <EntityID> " + this.promis.entityId + @" </EntityID>
+                    <TaxCodeForeignID>GrossUp 0%</TaxCodeForeignID>
+                    <TaxCodeName>GrossUp 0%</TaxCodeName>
+                    <TaxCodePercentage>0</TaxCodePercentage>
+                    <TaxCodeType>GrossUp</TaxCodeType>
+                    <ClientPercentageDefault>true</ClientPercentageDefault>
+                </TaxCode>
+                <TaxCode>
+                    <EntityID> " + this.promis.entityId + @" </EntityID>
+                    <TaxCodeForeignID>AU-TaxClaimable 0%</TaxCodeForeignID>
+                    <TaxCodeName>AU-TaxClaimable 0%</TaxCodeName>
+                    <TaxCodePercentage>0</TaxCodePercentage>
+                    <TaxCodeType>AU-TaxClaimable</TaxCodeType>
+                    <ClientPercentageDefault>true</ClientPercentageDefault>
+                </TaxCode>
+                <TaxCode>
+                    <EntityID>" + this.promis.entityId + @"</EntityID>
+                    <TaxCodeForeignID>AU-TaxClaimable 10%</TaxCodeForeignID>
+                    <TaxCodeName>AU-TaxClaimable 10%</TaxCodeName>
+                    <TaxCodePercentage>0.1</TaxCodePercentage>
+                    <TaxCodeType>AU-TaxClaimable</TaxCodeType>
+                    <ClientPercentageDefault>true</ClientPercentageDefault>
+                </TaxCode>
+            </TaxCodes>";
+
+            dynamic response = this.promis.postTaxCodes(xml);
+            return response;
+        }
+
+        dynamic sendContacts()
+        {
+            string xml = @"<Contacts>
+                <Contact>
+                    <EntityID> " + this.promis.entityId + @" </EntityID>
+                    <ContactForeignID>b40f61b3-b1ac-478f-909b-065b43f0efc9</ContactForeignID>
+                    <CompanyName>PF11</CompanyName>
+                    <ABN>12113628789</ABN>
+                    <Emails>
+                        <Email>pf11@promis.co</Email>
+                    </Emails>
+                    <Addresses>
+                        <Address>
+                            <Country>Australia</Country>
+                            <State>QLD</State>
+                            <PostCode>4567</PostCode>
+                            <City>Noosa Heads</City>
+                            <Street>PO Box 197</Street>
+                            <Type>STREET</Type>
+                        </Address>
+                        <Address>
+                            <Country>Australia</Country>
+                            <State>QLD</State>
+                            <PostCode>4567</PostCode>
+                            <City>Noosa Heads</City>
+                            <Street>PO Box 197</Street>
+                            <Type>POST</Type>
+                        </Address>
+                    </Addresses>
+                </Contact>
+            </Contacts>";
+
+            dynamic response = this.promis.postContacts(xml);
+            return response;
+        }
+
+        dynamic sendInvoice() {
+            string invoiceXmlPath = @"./invoice.xml";
+            if (System.Diagnostics.Debugger.IsAttached) invoiceXmlPath = @"../../../invoice.xml";
+
+            string invoicexml = File.ReadAllText(invoiceXmlPath);
+            //make the invoice unique
+            long epochTicks = new DateTime(1970, 1, 1).Ticks;
+            long now = ((DateTime.Today.Ticks - epochTicks) / TimeSpan.TicksPerSecond);
+            invoicexml = invoicexml.Replace("<InvoiceForeignID>0f07dbd5-60e8-4539-8665-6df58ada8f2a</InvoiceForeignID>", "<InvoiceForeignID>IN-FOREIGN-" + now + "</InvoiceForeignID>");
+            invoicexml = invoicexml.Replace("<InvoiceNumber>INV-0117</InvoiceNumber>", "<InvoiceNumber>INV-" + now + "</InvoiceNumber>");
+            invoicexml = invoicexml.Replace("[ENTITYID]", this.promis.entityId);
+            dynamic invoiceCreatedResponse = this.promis.postInvoice(invoicexml);
+            return invoiceCreatedResponse;
+        }
+
+
+    }
+}
---
+++ PromisConnector/Promis.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace PromisConnector
+{
+    public class Promis
+    {
+        string access_token = null;
+        public string entityId = null;
+
+        public Promis()
+        {
+            
+        }
+
+        public dynamic config()
+        {
+            string configpath = @"./config.json";
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                configpath = @"../../../config.json";
+            }
+            string configJson = File.ReadAllText(configpath);
+            dynamic json = JObject.Parse(configJson.ToString());
+            return json;
+        }
+
+        public string generatePassword(string password)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt());
+            return passwordHash;
+        }
+
+        public string login(string username, string password, string entityid)
+        {
+            string url = this.config()["api_host"] + "/login";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"UserName\": \"" + username + "\",\"Password\": \"" + password + "\",\"EntityID\": \"" + entityid + "\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            dynamic o = JObject.Parse(response.Content.ToString());
+
+            dynamic access_token = o["response"]["data"]["access_token"];
+            this.access_token = access_token.Value;
+            this.entityId = entityid;
+            return access_token.Value;
+        }
+
+        public dynamic createUser(string username, string password, string firstname = "", string middlename = "", string lastname = "")
+        {
+            string xml = @"<User>
+            <FirstName>" + firstname + @"</FirstName>
+            <MiddleName>" + middlename + @"</MiddleName>
+            <LastName>" + lastname + @"</LastName>
+            <UserName>" + username + @"</UserName>
+            <Password>" + password + @"</Password>
+            <SystemName>Tomsoft</SystemName>
+            </User>";
+            string url = this.config()["api_host"] + "/user";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public dynamic register(string entityName, string username, string password, string firstname = "", string middlename = "", string lastname = "")
+        {
+            string passwordHash = this.generatePassword(password);
+            dynamic user = this.createUser(username, passwordHash, firstname, middlename, lastname);
+
+            string xml = @"
+            <Register>
+                <User>
+                    <UserID>" + user._id + @"</UserID>
+                    <FirstName>Neil</FirstName>
+                    <LastName>Rogers</LastName>
+                    <DefaultRole>Subscriber</DefaultRole>
+                    <Entities>
+                        <Entity>
+                            <EntityForeignID>YourUniqueID</EntityForeignID>
+                            <OrgUser></OrgUser>
+                            <EntityName>" + entityName + @"</EntityName>
+                            <LegalEntityName>Testing Org1 Pty Ltd</LegalEntityName>
+                            <ABN></ABN>
+                            <GSTNumber></GSTNumber>
+                            <BranchCode></BranchCode>
+                            <EntityVerified>Yes</EntityVerified>
+                            <Tax>1</Tax>
+                            <HasABN>false</HasABN>
+                            <Country>AU</Country>
+                            <MainCurrency>AUD</MainCurrency>
+                            <Phone>12345678</Phone>
+                            <Mobile></Mobile>
+                            <Fax></Fax>                            
+                            <SystemURL></SystemURL>
+                            <MultiCurrency>false</MultiCurrency>
+                            <LineAmountType></LineAmountType>
+                            <SystemType>" + this.config()["systemtypeid"] + @"</SystemType>
+                            <PostalAddress>
+                                <Street>123 Test Street</Street>
+                                <Suburb>Testville</Suburb>
+                                <PostCode>1234</PostCode>
+                                <State>TEST</State>
+                                <Country>Australia</Country>
+                            </PostalAddress>
+                            <PhysicalAddress>
+                            <Street>123 Test Street</Street>
+                                <Suburb>Testville</Suburb>
+                                <PostCode>1234</PostCode>
+                                <State>TEST</State>
+                                <Country>Australia</Country>
+                            </PhysicalAddress>
+                        </Entity>
+                    </Entities>
+                </User>
+            </Register>";
+
+            string url = this.config()["api_host"] + "/register";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public dynamic getConnector()
+        {
+            string url = this.config()["api_host"] + "/connector";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("token", this.access_token);
+            request.AddHeader("Accept", "application/json");
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public dynamic postInvoice(string xml)
+        {
+            string url = this.config()["api_host"] + "/invoices";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("token", this.access_token);
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public dynamic postContacts(string xml)
+        {
+            string url = this.config()["api_host"] + "/contacts";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("token", this.access_token);
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public dynamic postTaxCodes(string xml)
+        {
+            string url = this.config()["api_host"] + "/taxcodes";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("token", this.access_token);
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public dynamic postAccounts(string xml)
+        {
+            string url = this.config()["api_host"] + "/accounts";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("token", this.access_token);
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            dynamic json = null;
+            if (response.ContentLength > 0)
+            {
+                json = JObject.Parse(response.Content.ToString());
+            }
+            return json;
+        }
+
+        public IRestResponse sendPDF()
+        {
+            string url = this.config()["api_host"] + "/connector/pdf?invoicenumber=INV00825724-b";
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("token", this.access_token);
+            request.AddHeader("Accept", "application/xml");
+
+            byte[] content = File.ReadAllBytes(@"Invoice.pdf");
+            request.AddFileBytes("pdf", content, "filename.pdf");
+            request.AddHeader("content-type", "multipart/form-data");
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+    }
+}
