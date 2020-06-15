6e7eed3ddb3c159ebc92ef63e8fe3fcb2ba4c1ba
Neil Rogers <neil.rogers@jetconvert.com>
2020-06-15 13:45:25 +1000

    * Promis.cs:
    * Program.cs: Remove unused using statements

--- PromisConnector/Program.cs
+++ PromisConnector/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using RestSharp;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.IO;
 
 namespace PromisConnector
